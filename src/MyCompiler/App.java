67b551e0bdc4f2d15ad50a2db639dcdef80d8ecb
GIL <gil.00000032610@unicap.br>
2023-04-06 13:26:11 -0300

    conferindo caractere Underscore

--- src/MyCompiler/App.java
+++ src/MyCompiler/App.java
@@ -1,6 +1,9 @@
 package MyCompiler;
 public class App {
     public static void main(String[] args) throws Exception {
-       
+       System.out.println(isLetra('_'));
+    }
+    public static boolean isLetra(char c) {
+        return (c >= 'a') && (c <= 'z');
     }
 }
