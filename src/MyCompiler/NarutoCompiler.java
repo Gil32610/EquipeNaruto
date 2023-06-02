e2de89c74c8dc16cf64787d2eeef1dd97f5674f3
GIL32610 <gil.00000032610@unicap.br>
2023-06-02 11:34:17 -0300

    NarutoCompiler

---
+++ src/MyCompiler/NarutoCompiler.java
@@ -0,0 +1,11 @@
+package MyCompiler;
+
+public class NarutoCompiler {
+    private Lexico lexico;
+    private Token current;
+
+    public NarutoCompiler(Lexico lexico) {
+        this.lexico = lexico;
+
+    }
+}
